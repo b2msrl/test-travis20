49b654ed8dac948714da08b1decf4e05b85c9e46
alevento <a@a.it>
2016-10-20 18:36:57 +0200

    Substitute

--- tests/config/sedCommandProvider.sh
+++ tests/config/sedCommandProvider.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sed -i "s/'providers' => \[/'providers' => \[ @@@namespacevendor\\\\@@@namespacepackage_name\\\\@@@providerpackage_nameServiceProvider::class,/g" ./config/app.php
+sed -i "s/'providers' => \[/'providers' => \[ Padosoft\\\\Testtravis20\\\\Testtravis20ServiceProvider::class,/g" ./config/app.php
